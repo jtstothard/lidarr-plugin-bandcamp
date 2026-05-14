27b70d1615c1c5ee92a28c9827e3debebc352f9a
jtstothard <jtstothard@gmail.com>
2026-05-14 16:27:06 +0100

    Set plugin owner to GitHub username

--- src/Lidarr.Plugin.Bandcamp/Plugin.cs
+++ src/Lidarr.Plugin.Bandcamp/Plugin.cs
@@ -3,7 +3,7 @@ namespace NzbDrone.Core.Plugins
     public class BandcampPlugin : Plugin
     {
         public override string Name => "Bandcamp";
-        public override string Owner => "lidarr-plugin-bandcamp";
+        public override string Owner => "jtstothard";
         public override string GithubUrl => "https://github.com/jtstothard/lidarr-plugin-bandcamp";
     }
 }
